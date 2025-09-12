30d5c74909a59072b86d17098103a12393143d52
Your Name <kshitizs391@gmail.com>
2025-09-12 10:34:07 +0530

    js

---
+++ javascript1/array.js
@@ -0,0 +1,48 @@
+//
+//  let arr=[0,1,2,3,4,5]
+// console.log(arr);
+
+// console.log(arr[3]);
+
+// console.log(arr.push(6));
+// console.log(arr.pop());
+
+// console.log(arr.includes(4));
+// console.log(arr.indexOf(4));
+
+// let arr2=arr.join()
+// console.log(arr2);
+
+//slice 
+// let arr2 =arr.slice(1,3)
+// console.log(arr2);
+
+// let arr2 =arr.splice(1,3)
+// console.log(arr2);
+
+// let lang =["java","css","js"]
+// let lang1=["mongo","sql","express"]
+// lang.push(lang1)
+// console.log(lang);
+
+// let new2 =lang.concat(lang1)
+// console.log(new2);
+
+
+// let new2 =[...lang, ...lang1] spread operator
+// console.log(new2);
+
+
+// let new2=[1,2,3[4,5,6],7[3,5,6],9]
+// let new1=new2.flat(Infinity)
+// console.log(new1);
+
+
+// let a=20
+// let b=23
+// let c=34
+// console.log(Array.of(a,b,c));   for making arrays 
+
+
+
+
---
+++ javascript1/objects.js
@@ -0,0 +1,41 @@
+//
+let obj={
+    name:"kshitiz",
+    age:21,
+    roll:2300320150030,
+    branch:"computer engineering ",
+    Email1:"kshitizs391@gmail.com",
+    MOBILE:"8445XXXXX",
+   
+}
+// console.log(obj);
+
+// obj.Email1="kshitizs516"
+// console.log(obj);
+
+
+// let user ={}
+// user.id="123.sdd"
+// user.name="jss"
+// console.log(user);
+
+
+// let user ={
+//     email:"kshitizsharma",
+//     fullname:{
+//         surname:"sharma",
+//         lastanme:"he"
+//     }
+// }
+// console.log(user.fullname.surname);
+
+
+// let obj1={1:"A",2:"b"}
+// let obj2={4:"s",5:"y"}
+// // let obj3=Object.assign({},obj1,obj2)// for adding objects  {}iske andar dono objects ja rhe hai
+// // console.log(obj3);
+
+// let obj3={...obj1,...obj2}
+// console.log(obj3);
+
+// console.log(Object.keys(obj));
